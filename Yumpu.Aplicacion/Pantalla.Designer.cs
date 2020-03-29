ce10e0c87bffc8899789c2dc1ad54a851d055125
Victor Hugo Paucas Navarro <vhpaucas@gmail.com>
2020-03-29 16:47:58 -0500

    Add project files.

---
+++ Yumpu.Aplicacion/Pantalla.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Yumpu.Aplicacion
+{
+    partial class Pantalla
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Pantalla));
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.btnDescargar = new System.Windows.Forms.Button();
+            this.lblRuta = new System.Windows.Forms.Label();
+            this.txtRuta = new System.Windows.Forms.TextBox();
+            this.btnRuta = new System.Windows.Forms.Button();
+            this.fbdRuta = new System.Windows.Forms.FolderBrowserDialog();
+            this.pgbProgreso = new System.Windows.Forms.ProgressBar();
+            this.lblProgreso = new System.Windows.Forms.Label();
+            this.mstContenedor = new System.Windows.Forms.MenuStrip();
+            this.tsmAyuda = new System.Windows.Forms.ToolStripMenuItem();
+            this.mstContenedor.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(34, 90);
+            this.lblCodigo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(72, 19);
+            this.lblCodigo.TabIndex = 0;
+            this.lblCodigo.Text = "Código:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.BackColor = System.Drawing.Color.White;
+            this.txtCodigo.Location = new System.Drawing.Point(105, 87);
+            this.txtCodigo.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCodigo.MaxLength = 20;
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(463, 26);
+            this.txtCodigo.TabIndex = 1;
+            this.txtCodigo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnDescargar
+            // 
+            this.btnDescargar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDescargar.Location = new System.Drawing.Point(448, 121);
+            this.btnDescargar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDescargar.Name = "btnDescargar";
+            this.btnDescargar.Size = new System.Drawing.Size(120, 34);
+            this.btnDescargar.TabIndex = 0;
+            this.btnDescargar.TabStop = false;
+            this.btnDescargar.Text = "Descargar";
+            this.btnDescargar.UseVisualStyleBackColor = true;
+            this.btnDescargar.Click += new System.EventHandler(this.btnDescargar_Click);
+            // 
+            // lblRuta
+            // 
+            this.lblRuta.AutoSize = true;
+            this.lblRuta.Location = new System.Drawing.Point(34, 56);
+            this.lblRuta.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblRuta.Name = "lblRuta";
+            this.lblRuta.Size = new System.Drawing.Size(54, 19);
+            this.lblRuta.TabIndex = 1;
+            this.lblRuta.Text = "Ruta:";
+            // 
+            // txtRuta
+            // 
+            this.txtRuta.BackColor = System.Drawing.Color.White;
+            this.txtRuta.Enabled = false;
+            this.txtRuta.Location = new System.Drawing.Point(105, 53);
+            this.txtRuta.Margin = new System.Windows.Forms.Padding(4);
+            this.txtRuta.Name = "txtRuta";
+            this.txtRuta.Size = new System.Drawing.Size(463, 26);
+            this.txtRuta.TabIndex = 2;
+            this.txtRuta.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnRuta
+            // 
+            this.btnRuta.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRuta.Location = new System.Drawing.Point(576, 48);
+            this.btnRuta.Margin = new System.Windows.Forms.Padding(4);
+            this.btnRuta.Name = "btnRuta";
+            this.btnRuta.Size = new System.Drawing.Size(120, 34);
+            this.btnRuta.TabIndex = 0;
+            this.btnRuta.Text = "Seleccionar";
+            this.btnRuta.UseVisualStyleBackColor = true;
+            this.btnRuta.Click += new System.EventHandler(this.btnRuta_Click);
+            // 
+            // pgbProgreso
+            // 
+            this.pgbProgreso.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.pgbProgreso.Location = new System.Drawing.Point(38, 181);
+            this.pgbProgreso.Name = "pgbProgreso";
+            this.pgbProgreso.Size = new System.Drawing.Size(658, 23);
+            this.pgbProgreso.TabIndex = 3;
+            // 
+            // lblProgreso
+            // 
+            this.lblProgreso.AutoSize = true;
+            this.lblProgreso.Location = new System.Drawing.Point(34, 159);
+            this.lblProgreso.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblProgreso.Name = "lblProgreso";
+            this.lblProgreso.Size = new System.Drawing.Size(0, 19);
+            this.lblProgreso.TabIndex = 4;
+            // 
+            // mstContenedor
+            // 
+            this.mstContenedor.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mstContenedor.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmAyuda});
+            this.mstContenedor.Location = new System.Drawing.Point(0, 0);
+            this.mstContenedor.Name = "mstContenedor";
+            this.mstContenedor.Size = new System.Drawing.Size(734, 27);
+            this.mstContenedor.TabIndex = 5;
+            this.mstContenedor.Text = "menuStrip1";
+            // 
+            // tsmAyuda
+            // 
+            this.tsmAyuda.Name = "tsmAyuda";
+            this.tsmAyuda.Size = new System.Drawing.Size(66, 23);
+            this.tsmAyuda.Text = "Ayuda";
+            this.tsmAyuda.Click += new System.EventHandler(this.tsmAyuda_Click);
+            // 
+            // Pantalla
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(734, 230);
+            this.Controls.Add(this.lblProgreso);
+            this.Controls.Add(this.pgbProgreso);
+            this.Controls.Add(this.btnRuta);
+            this.Controls.Add(this.txtRuta);
+            this.Controls.Add(this.lblRuta);
+            this.Controls.Add(this.btnDescargar);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.lblCodigo);
+            this.Controls.Add(this.mstContenedor);
+            this.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.mstContenedor;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Pantalla";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Yumpu";
+            this.mstContenedor.ResumeLayout(false);
+            this.mstContenedor.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Button btnDescargar;
+        private System.Windows.Forms.Label lblRuta;
+        private System.Windows.Forms.TextBox txtRuta;
+        private System.Windows.Forms.Button btnRuta;
+        private System.Windows.Forms.FolderBrowserDialog fbdRuta;
+        private System.Windows.Forms.ProgressBar pgbProgreso;
+        private System.Windows.Forms.Label lblProgreso;
+        private System.Windows.Forms.MenuStrip mstContenedor;
+        private System.Windows.Forms.ToolStripMenuItem tsmAyuda;
+    }
+}
+
---
+++ Yumpu.Aplicacion/Pantalla.cs
@@ -0,0 +1,165 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows.Forms;
+
+namespace Yumpu.Aplicacion
+{
+    public partial class Pantalla : Form
+    {
+        int CantidadImagenes;
+        string Codigo;
+        string Directorio;
+
+        public Pantalla()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDescargar_Click(object sender, EventArgs e)
+        {
+            if (ValidarCampos())
+            {
+                try
+                {
+                    Codigo = txtCodigo.Text.Trim();
+
+                    using (WebClient webClient = new WebClient())
+                    {
+                        var downloadString = webClient.DownloadString("https://www.yumpu.com/en/document/json/" + txtCodigo.Text.Trim());
+                        JObject jObject = JObject.Parse(downloadString);
+                        CantidadImagenes = jObject["document"]["pages"].Count();
+
+                        if (CantidadImagenes > 0)
+                        {
+                            //Directorio = Directory.GetCurrentDirectory() + "/" + "Imagenes - " + txtCodigo.Text.Trim() + " - " + DateTime.Now.ToString("yyyyMMddhhmmss");
+                            //Directory.CreateDirectory(Directorio);
+
+                            #region "Descargar Imagenes"
+                            FormatearProgreso("Descargando imagenes ...");
+                            try
+                            {
+                                for (int Indice = 1; Indice <= CantidadImagenes; Indice++)
+                                {
+                                    webClient.DownloadFile(new Uri("https://img.yumpu.com/" + Codigo + "/" + Convert.ToString(Indice) + "/1238x1600"), Directorio + "/" + Indice + ".jpg");
+                                    pgbProgreso.PerformStep();
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                lblProgreso.Text = "Error al descargar imagenes.";
+                                MessageBox.Show("Error al descargar imagenes.", "Yumpu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                return;
+                            }
+                            #endregion "Descargar Imagenes"
+
+                            #region "Convertir Imagenes a PDF"
+                            FormatearProgreso("Convertiendo imagenes a PDF ...");
+                            Document document = new Document();
+
+                            try
+                            {
+                                PdfWriter.GetInstance(document, new FileStream(Directorio + "/" + Codigo + " - " + DateTime.Now.ToString("yyyyMMddhhmmss") + ".pdf", FileMode.Create));
+                                document.Open();
+
+                                for (int Indice = 1; Indice <= CantidadImagenes; Indice++)
+                                {
+                                    Image image = Image.GetInstance(Directorio + "/" + Indice + ".jpg");
+                                    image.ScaleAbsolute(570, 750);
+                                    document.Add(image);
+                                    pgbProgreso.PerformStep();
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                lblProgreso.Text = "Error al convertir imagenes a pdf.";
+                                MessageBox.Show("Error al convertir imagenes a pdf.", "Yumpu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                return;
+                            }
+                            finally
+                            {
+                                document.Close();
+                            }
+                            #endregion "Convertir Imagenes a PDF"
+
+                            #region "Eliminar Imagenes"
+                            FormatearProgreso("Eliminando imagenes ...");
+                            try
+                            {
+                                for (int Indice = 1; Indice <= CantidadImagenes; Indice++)
+                                {
+                                    if (File.Exists(Directorio + "/" + Indice + ".jpg"))
+                                    {
+                                        File.Delete(Directorio + "/" + Indice + ".jpg");
+                                    }
+                                    pgbProgreso.PerformStep();
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                lblProgreso.Text = "Error al eliminar imagenes.";
+                                MessageBox.Show("Error al eliminar imagenes.", "Yumpu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                return;
+                            }
+                            #endregion "Eliminar Imagenes"
+
+                            FormatearProgreso("");
+                            MessageBox.Show("Se descargó la revista correctamente.", "Yumpu", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Error al buscar código, verifique que el código exista.", "Yumpu", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void btnRuta_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            folderBrowserDialog.ShowNewFolderButton = true;
+            DialogResult result = folderBrowserDialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                txtRuta.Text = folderBrowserDialog.SelectedPath;
+                Directorio = folderBrowserDialog.SelectedPath;
+            }
+        }
+
+        private void FormatearProgreso(string Texto)
+        {
+            pgbProgreso.Visible = true;
+            pgbProgreso.Minimum = 1;
+            pgbProgreso.Maximum = CantidadImagenes;
+            pgbProgreso.Value = 1;
+            pgbProgreso.Step = 1;
+
+            lblProgreso.Text = Texto;
+        }
+
+        private bool ValidarCampos()
+        {
+            if (txtRuta.Text.Trim().Length <= 0)
+            {
+                MessageBox.Show("Falta seleccionar la ruta de descarga.", "Yumpu", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            if (txtCodigo.Text.Trim().Length <= 0)
+            {
+                MessageBox.Show("Falta ingresar el código de revista.", "Yumpu", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return false;
+            }
+            return true;
+        }
+
+        private void tsmAyuda_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Autor: Paucas Navarro, Victor Hugo \nCorreo: vhpaucas@gmail.com \nRepositorio: www.github.com/victorpaucas", "Yumpu", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
